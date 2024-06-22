9975329f41824670619ae528cce00873c8c03bc4
linarcx <linarcx@gmail.com>
2024-06-22 23:19:27 +0200

    updated

--- src/main.c
+++ src/main.c
@@ -7,40 +7,74 @@
 void
 generateDir(const char* dirName, unsigned int mode)
 {
-  if (mkdir(dirName, mode) == 0) 
-  {
-    printf("INFO --> Directory: %s created successfully\n", dirName);
-  } 
-  else 
+  if (-1 == mkdir(dirName, mode))
   {
     printf("ERROR --> generateDir(): can't create %s, errno: %d", dirName, errno);
     exit(EXIT_FAILURE);
-  }
+  } 
 }
 
 void
-generateFile(const char* fileName, const char* mode)
+generateFile(const char* fileName, const char* mode, const char* text)
 {
-  if (fopen(fileName, mode)) 
-  {
-    printf("INFO --> File: %s created successfully\n", fileName);
-  }
-  else
+  FILE *file = fopen(fileName, mode);
+  if (NULL == file) 
   {
     printf("ERROR --> generateFile(): can't create %s, errno: %d", fileName, errno);
     exit(EXIT_FAILURE);
   }
-}
 
+  fprintf(file, text);
+
+  if (EOF == fclose(file)) 
+  {
+    printf("ERROR --> generateFile(): can't close %s, errno: %d", fileName, errno);
+    exit(EXIT_FAILURE);
+  }
+}
 
 int
 main(int argc, char** argv)
 {
-  generateDir("src", 0755);
-  generateFile("src/main.c", "w");
+  const char* projectName = "Mitra";
+  const char* projectDesc = "A project generator";
 
-  generateFile("README.md", "w");
-  generateFile("project.sh", "w");
-  generateFile(".gitignore", "w");
+  generateDir("src", 0755);
+  generateFile("src/main.c", "w", "int\n\
+main(int argc, char** argv)\n\
+{\n\
+  return 0;\n\
+}\n");
+
+  generateFile("README.md", "w", "# %s\n%s", projectName, projectDesc);
+
+
+  //generateFile("project.sh", "w", "");
+  //generateFile(".gitignore", "w", "");
   return 0;
 }
+
+  //char* fileName = "src/main.c";
+  //char* mode = "w";
+  //FILE *file = fopen(fileName, mode);
+  //if (NULL == file) 
+  //{
+  //  printf("ERROR --> generateFile(): can't create %s, errno: %d", fileName, errno);
+  //  exit(EXIT_FAILURE);
+  //}
+
+//  fprintf(file, 
+//          "int\n\
+//main(int argc, char** argv)\n\
+//{\n\
+//  return 0;\n\
+//}\n");
+//
+//  if (EOF == fclose(file)) 
+//  {
+//    printf("ERROR --> generateFile(): can't close %s, errno: %d", fileName, errno);
+//    exit(EXIT_FAILURE);
+//  }
+
+
+
